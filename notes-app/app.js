7c265206dfb3815f12e830f7c8070b902579ad07
Ahmad Hamed Rangeen <ahmadrangeen@Ahmads-MacBook-Pro.local>
2019-06-01 17:50:29 -0700

    using chalk library in my project to print in color

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,13 +1,19 @@
-// load in npm module
-const validator = require("validator");
-
 // use require to load in the function from notes.js
 const getNotes = require("./notes.js");
 
+// load in the library chalk
+const chalk = require("chalk");
 // now we can use funciton
 const message = getNotes();
 
 console.log(message);
 
-console.log(validator.isEmail("ahmad@gmail.com"));
-console.log(validator.isURL("https://mead.io"));
+console.log(chalk.bold.green("Success!"));
+
+/* 
+    Challenge: Use the Chalk library in your project
+        - install version 2.4.1 of chalk 
+        - load chalk into app.js
+        - use it to print the string "success!" to the console in green 
+        - test it 
+*/
