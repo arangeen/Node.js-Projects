0e00961320d324f81c2f0380d6a3b0839099045e
Ahmad Hamed Rangeen <ahmadrangeen@Ahmads-MacBook-Pro.local>
2019-06-01 17:19:22 -0700

    importing npm modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,5 @@
-/*
-  Defining and using a function in a new file 
-    1. create a new file called notes.js
-    2. create getNotes function thata returns "Your notes..."
-    3. Export getNotes function 
-    4. from app.js, load in and call the fucntion printing message to console
-    
-*/
+// load in npm module
+const validator = require("validator");
 
 // use require to load in the function from notes.js
 const getNotes = require("./notes.js");
@@ -14,3 +8,6 @@ const getNotes = require("./notes.js");
 const message = getNotes();
 
 console.log(message);
+
+console.log(validator.isEmail("ahmad@gmail.com"));
+console.log(validator.isURL("https://mead.io"));
