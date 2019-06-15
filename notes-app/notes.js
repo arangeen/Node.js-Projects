4cdc6bf3cbe841dbc5af8c3aa7cf4edf7faaeb3d
Ahmad Hamed Rangeen <ahmadrangeen@Ahmads-MacBook-Pro.local>
2019-06-15 13:47:02 -0700

    made sure user cant add duplicate notes

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -7,12 +7,22 @@ const getNotes = function() {
 const addNote = function(title, body) {
   const notes = loadNotes();
 
-  notes.push({
-    title: title,
-    body: body
+  // store all duplicate titles in here
+  const duplicateNotes = notes.filter(function(note) {
+    return note.title === title;
   });
 
-  saveNotes(notes);
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body
+    });
+
+    saveNotes(notes);
+    console.log("New note added!");
+  } else {
+    console.log("NOte title already exists!");
+  }
 };
 
 const saveNotes = function(notes) {
