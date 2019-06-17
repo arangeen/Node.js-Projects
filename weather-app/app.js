e73943e2d3eb0e93b553107a01c89ef8e5ea00ff
Ahmad Hamed Rangeen <ahmadrangeen@Ahmads-MacBook-Pro.local>
2019-06-17 16:23:22 -0700

    geocoding for Los Angeles

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,7 @@
 const request = require("request");
 
 const url =
-  "https://api.darksky.net/forecast/45c8fd671c3c1dbd4b83dcc55bc51803/37.8267,-122.4233?units=si&lang=es";
+  "https://api.darksky.net/forecast/45c8fd671c3c1dbd4b83dcc55bc51803/37.8267,-122.4233";
 
 // setting json to true is saying we would like a request to parse this as json
 request({ url: url, json: true }, (error, response) => {
@@ -16,8 +16,21 @@ request({ url: url, json: true }, (error, response) => {
   );
 });
 
+//Geocoding
+// Address -> Lat/Lang -> Weather
+
 /**
- * Goal: Print a small forecast to the user
- *
- *  1. Print: "The temperature outside is currently 58.55 degrees out. There is a 0% chance of rain"
+ * Goal: Print the lat/long for Los Angeles
+ *    1. FIre off a new request to the URL (mapbox.com)
+ *    2. Have the request module parse it as JSON
+ *    3. Print both the lat and long to the terminal
+ *    4. test work
  */
+
+const geocodeURL =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYXJhbmdlZW4iLCJhIjoiY2p4MHpneXY4MDJldDRibm9ocHkxOGJmdyJ9.i4ZNm_xGY2TtV8VwwbuSlQ&limit=1";
+request({ url: geocodeURL, json: true }, (error, response) => {
+  const latitude = response.body.features[0].center[1];
+  const longitude = response.body.features[0].center[0];
+  console.log(latitude, longitude);
+});
