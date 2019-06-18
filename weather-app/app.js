f41d98d65eac296ba24c676a31e0cb9511090537
Ahmad Hamed Rangeen <ahmadrangeen@Ahmads-MacBook-Pro.local>
2019-06-17 18:31:19 -0700

    incorporated callback function to geocode function for weather-app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,8 @@
 const request = require("request");
+const geocode = require("./utils/geocode");
 
-const url =
-  "https://api.darksky.net/forecast/45c8fd671c3c1dbd4b83dcc55bc51803/37.8267,-122.4233";
+// const url =
+//   "https://api.darksky.net/forecast/45c8fd671c3c1dbd4b83dcc55bc51803/37.8267,-122.4233";
 
 // // setting json to true is saying we would like a request to parse this as json
 // request({ url: url, json: true }, (error, response) => {
@@ -24,24 +25,21 @@ const url =
 //Geocoding
 // Address -> Lat/Lang -> Weather
 
-/**
- * Goal: Handle Errors for geocoding request
- *    1. setup an error handler for low-level errors
- *    2. test by diabling network request and running the app
- *    3. setup error handling for no matching results
- *    4. test by altering search term and running the app
- */
+// const geocodeURL =
+//   "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYXJhbmdlZW4iLCJhIjoiY2p4MHpneXY4MDJldDRibm9ocHkxOGJmdyJ9.i4ZNm_xGY2TtV8VwwbuSlQ&limit=1";
+// request({ url: geocodeURL, json: true }, (error, response) => {
+//   if (error) {
+//     console.log("Unable to connect to location services!");
+//   } else if (response.body.features.length === 0) {
+//     console.log("Unable to find location. Try another search!");
+//   } else {
+//     const latitude = response.body.features[0].center[1];
+//     const longitude = response.body.features[0].center[0];
+//     console.log(latitude, longitude);
+//   }
+// });
 
-const geocodeURL =
-  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYXJhbmdlZW4iLCJhIjoiY2p4MHpneXY4MDJldDRibm9ocHkxOGJmdyJ9.i4ZNm_xGY2TtV8VwwbuSlQ&limit=1";
-request({ url: geocodeURL, json: true }, (error, response) => {
-  if (error) {
-    console.log("Unable to connect to location services!");
-  } else if (response.body.features.length === 0) {
-    console.log("Unable to find location. Try another search!");
-  } else {
-    const latitude = response.body.features[0].center[1];
-    const longitude = response.body.features[0].center[0];
-    console.log(latitude, longitude);
-  }
+geocode("Boston", (error, data) => {
+  console.log("Error", error);
+  console.log("Data", data);
 });
