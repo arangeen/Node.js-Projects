2bd4fb18ad71e6293c1e478574bcec21c48d8dd3
Ahmad Hamed Rangeen <ahmadrangeen@Ahmads-MacBook-Pro.local>
2019-06-20 17:39:18 -0700

    new goal posted

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,15 +1,27 @@
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-//Geocoding
-// Address -> Lat/Lang -> Weather
+/**
+ * Goal: Accept location via command line argument
+ *  1. access the command line argument without yargs
+ *  2. use the string value as the input for geocode
+ *  3. only geocode if a location was provided
+ *  4. Test you work with a couple locations
+ */
 
 geocode("Boston", (error, data) => {
-  console.log("Error", error);
-  console.log("Data", data);
+  if (error) {
+    return console.log(error);
+  }
+
   //callback chaining: chaining together multiple callbacks to do mulitiple things in a specific order
-  forecast(data.latitude, data.longitude, (error, data) => {
-    console.log("Error", error);
-    console.log("Data", data);
+  forecast(data.latitude, data.longitude, (error, forecastData) => {
+    if (error) {
+      return console.log(error);
+    }
+
+    //print location and forecast data here
+    console.log(data.location);
+    console.log(forecastData);
   });
 });
