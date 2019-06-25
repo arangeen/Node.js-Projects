f62ae5beb029b174364e85e1d17c86fe54662ec7
Ahmad Hamed Rangeen <ahmadrangeen@Ahmads-MacBook-Pro.local>
2019-06-24 21:37:49 -0700

    set up routes to render html and json objects

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,33 +1,44 @@
 //load in express, configure it to serve something up and start server
-
 const express = require("express");
 const app = express();
 
-// ex. app.com , app.com/help , app.com/about
-
 // req is short for request, res is short for response
+// sending back html data to the browser
 app.get("", (req, res) => {
-  res.send("Hello Express!");
+  res.send("<h1> Weather</h1>");
 });
 
+// send back json object
 app.get("/help", (req, res) => {
-  res.send("Help Page");
+  res.send([
+    {
+      name: "Ahmad",
+      age: 25
+    },
+    {
+      name: "Bemi",
+      age: 26
+    }
+  ]);
 });
+
+/**Goal: Updata routes
+ *  1. Setup about route to render a title with html
+ *  2. Setup a weather route to send back json
+ *      - Object with forecast and location strings
+ *  3. Test your work
+ */
 
 app.get("/about", (req, res) => {
-  res.send("About Page");
+  res.send("<h1>About</h1>");
 });
 
 app.get("/weather", (req, res) => {
-  res.send("Weather Page");
+  res.send({
+    forecast: "It is sunny",
+    location: "California"
+  });
 });
-
-/**
- * Goal: Setup two new routes
- *  1. Setup an about route and render a page title
- *  2. weather route and render page title
- *  3. test
- */
 
 // now start the server up
 // port numeber 3000
