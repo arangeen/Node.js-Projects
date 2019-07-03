ba90e4dfa78c1b143d31830366e09761b720133a
Ahmad Hamed Rangeen <ahmadrangeen@Ahmads-MacBook-Pro.local>
2019-07-03 16:00:50 -0700

    Used handlebaebars to render dynamic contents

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -7,11 +7,39 @@ const express = require("express");
 const app = express();
 
 const publicDirectoryPath = path.join(__dirname, "../public");
+
+// getting handlebars set up
+app.set("view engine", "hbs");
+
 // a way to customize server to serve up the folder we want
 app.use(express.static(publicDirectoryPath));
 
-//Goal: Create two more HtML  files
-//  - about page and help page
+// setting up route to use index.hbs
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Ahmad Rangeen"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Ahmad Rangeen"
+  });
+});
+
+/**
+ * Goal: Create a template for help page
+ *  1. setup a help template to render a help message to the screen
+ *  2. Setup a help route and render the template with an example message
+ *  3. Visit the route in the browser and see help message
+ */
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This is some helpful text."
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send({
@@ -25,5 +53,3 @@ app.get("/weather", (req, res) => {
 app.listen(3000, () => {
   console.log("Server is up on port 3000");
 });
-
-// to view page, go to chrome and search 'localhost:3000'
