7643ab63c8b1b2fdd85fbcafff74daa55781c59c
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-07-06 15:57:31 -0700

    used fetch api to fetch the weather and print it to console

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,22 @@
 console.log("Client side javascript side is loaded");
+
+/**
+ * Goal: Fetch Weather!
+ *  1. setup a call to fetch to fetch weather form Boston
+ *  2. Get the parse JSON response
+ *      - if no errror property, print location and forecast to console
+ *  3. Refresh browser and test work
+ */
+
+fetch("http://localhost:3000/weather?address=Boston").then(response => {
+  response.json().then(data => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      console.log(data.location);
+      console.log(data.forecast);
+    }
+  });
+});
+
+// explored the fetch api,
