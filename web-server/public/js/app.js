29c819376f80717afe19ac114853f69fa71f43e9
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-07-06 16:27:01 -0700

    created a search form and printed forecast to console

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,22 +1,31 @@
 console.log("Client side javascript side is loaded");
 
-/**
- * Goal: Fetch Weather!
- *  1. setup a call to fetch to fetch weather form Boston
- *  2. Get the parse JSON response
- *      - if no errror property, print location and forecast to console
- *  3. Refresh browser and test work
- */
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
 
-fetch("http://localhost:3000/weather?address=Boston").then(response => {
-  response.json().then(data => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
-    }
+// first argument is name of event we want to listen to
+// funtion will run everytime form is submitted
+weatherForm.addEventListener("submit", event => {
+  // browser wont refresh eveytime you submit the form
+  event.preventDefault();
+
+  const location = search.value;
+
+  fetch("http://localhost:3000/weather?address=" + location).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        console.log(data.location);
+        console.log(data.forecast);
+      }
+    });
   });
 });
 
-// explored the fetch api,
+/**
+ * Goal: use input value to get weather
+ *  1. migrate fetch call into the submit callback
+ *  2. Use the search text as address query string value
+ *  3. Submit the form with a valid and incalid value test
+ */
