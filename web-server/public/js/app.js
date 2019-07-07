5cc9adc46213dc602f0ba30e712c9b119ececbb8
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-07-06 17:32:39 -0700

    wired up the user Interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,6 +2,17 @@ console.log("Client side javascript side is loaded");
 
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
+//targeting id="message-1". To target class name, start with "."
+const messageOne = document.querySelector("#message-1");
+
+const messageTwo = document.querySelector("#message-2");
+/**
+ * Goal: Render content to paragraphs
+ *  1. select the second message p from javascript
+ *  2. just before fetch, render loading message and empty p
+ *  3. if error, render error
+ *  4. no error, render location and forecast
+ */
 
 // first argument is name of event we want to listen to
 // funtion will run everytime form is submitted
@@ -11,21 +22,17 @@ weatherForm.addEventListener("submit", event => {
 
   const location = search.value;
 
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
+
   fetch("http://localhost:3000/weather?address=" + location).then(response => {
     response.json().then(data => {
       if (data.error) {
-        console.log(data.error);
+        messageOne.textContent = data.error;
       } else {
-        console.log(data.location);
-        console.log(data.forecast);
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
       }
     });
   });
 });
-
-/**
- * Goal: use input value to get weather
- *  1. migrate fetch call into the submit callback
- *  2. Use the search text as address query string value
- *  3. Submit the form with a valid and incalid value test
- */
